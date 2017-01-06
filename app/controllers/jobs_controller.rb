a8cb635c64df887588db4c412f851dadd8acc97a
linjiaxin <linjiaxin405@gmail.com>
2017-01-06 20:20:13 +0800

    安装好simple_form

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -17,14 +17,27 @@ class JobsController < ApplicationController
 
   def create
     @job = Job.new(job_params)
-    @job.save
-    redirect_to jobs_path
+    if @job.save
+      redirect_to jobs_path
+    else
+      render :new
+    end
   end
 
   def update
     @job = Job.find(params[:id])
-    @job.update(job_params)
-    redirect_to jobs_path, notice: "Update Success"
+    if @job.update(job_params)
+      redirect_to jobs_path, notice: "Update Success"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id])
+    @job.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to jobs_path
   end
 
   private
