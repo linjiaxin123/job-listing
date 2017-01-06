a81ad86a230be3b39ea5c51edf9c4b9bc5d43992
linjiaxin <linjiaxin405@gmail.com>
2017-01-06 20:11:02 +0800

    添加修改功能

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,6 +3,14 @@ class JobsController < ApplicationController
     @jobs = Job.all
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end
+
+  def edit
+    @job = Job.find(params[:id])
+  end
+
   def new
     @job = Job.new
   end
@@ -11,6 +19,12 @@ class JobsController < ApplicationController
     @job = Job.new(job_params)
     @job.save
     redirect_to jobs_path
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    @job.update(job_params)
+    redirect_to jobs_path, notice: "Update Success"
   end
 
   private
