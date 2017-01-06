82ba2f990e3b81a0acca8371564e280e95a66aaf
linjiaxin <linjiaxin405@gmail.com>
2017-01-06 21:08:20 +0800

    增加限制按钮

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user! , only: [:new, :edit, :create, :update, :destroy]
-  before_filter :require_is_admin
 
   def index
     @jobs = Job.all
@@ -41,13 +40,6 @@ class JobsController < ApplicationController
     @job.destroy
     flash[:alert] = "Group deleted"
     redirect_to jobs_path
-  end
-
-  def require_is_admin
-    unless current_user.admin?
-      flash[:alert] = 'You are not admin'
-      redirect_to root_path
-    end
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   def admin?
-    email == 'aaa@aaa.com'
+    is_admin
   end
 end
