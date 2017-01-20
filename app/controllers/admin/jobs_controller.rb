b1fbdcf493f9bc763e70f4b3640c31c976ce2c37
linjiaxin <linjiaxin405@gmail.com>
2017-01-20 22:35:05 +0800

    ss

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -8,7 +8,7 @@ class Admin::JobsController < ApplicationController
   end
 
   def index
-    @jobs = Job.all.paginate(:page => params[:page], :per_page => 10)
+    @jobs = Job.all
   end
 
   def new
