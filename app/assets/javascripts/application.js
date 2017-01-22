bc2dda70c4e1a92a6bd87ac057367ab47d6a84a0
linjiaxin <linjiaxin405@gmail.com>
2017-01-22 21:55:38 +0800

    1月22日修改

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,3 +21,9 @@ $(document).ready(function() {
   $('#myCarousel').carousel({interval: 2000});
 
 });
+
+
+$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+});
