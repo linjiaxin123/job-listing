771008189cd979854b92910d8fa807d2facb565d
linjiaxin <linjiaxin405@gmail.com>
2017-01-26 22:14:40 +0800

    solve search bug

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -57,26 +57,29 @@ class JobsController < ApplicationController
 
   def search
     if @query_string.present?
-      search_result = Job.ransack(@search_criteria).result(:distinct => true)
+      search_result = Job.published.ransack(@search_criteria).result(:distinct => true)
       @jobs = search_result.paginate(:page => params[:page], :per_page => 6 )
     end
+  end
+
+  protected
+
+  def validate_search_key
+    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "")
+    if params[:q].present?
+      @search_criteria =  {title_cont: @query_string}
+    end
+  end
+
+
+  def search_criteria(query_string)
+    { :title_cont => query_string }
   end
 
   private
 
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
-  end
-
-  protected
-
-  def validate_search_key
-    @query_string = params[:q].gsub(/\\|\'|\/|\?/, "") if params[:q].present?
-    @search_criteria = search_criteria(@query_stringß)
-  end
-
-  def search_criteria(query_string)
-    { :title_cont => query_string}
   end
 
 end
